efc64f490cac129cdcf995090dcefbaeeb4af263
ltvinh9899 <37173634+ltvinh9899@users.noreply.github.com>
2018-09-19 23:14:06 +0700

    Update Student.java

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -3,14 +3,45 @@ package week1;
 public class Student {
 
     // TODO: khai báo các thuộc tính cho Student
-
+     private String name;
+     private String id;
+     private String group;
+     private String email;
+    
     // TODO: khai báo các phương thức getter, setter cho Student
-
+     public void setName(String name){
+        this.name = name;
+     }
+     public String getName(){
+        return name;
+     }
+    public void setId(String id){
+        this.id = id;
+    }
+    public String getId(){
+        return id;
+    }
+    public void setGroup(String group){
+        this.group = group;
+    }
+    public String getGroup(){
+         return group;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public String getEmail(){
+        return email;
+    }
     /**
      * Constructor 1
      */
-    Student() {
+   public Student() {
         // TODO:
+        name = "Student";
+        id = "000";
+        group = "INT22041";
+        email = "uet@vnu.edu.vn";
     }
 
     /**
@@ -21,6 +52,10 @@ public class Student {
      */
     Student(String n, String sid, String em) {
         // TODO:
+        this.name = n;
+        this.id = sid;
+        this.group = "INT22041";
+        this.email = em;
     }
 
     /**
@@ -29,10 +64,16 @@ public class Student {
      */
     Student(Student s) {
         // TODO:
+        this.name = s.getName();
+        this.id = s.getId();
+        this.group = s.getGroup();
+        this.email = s.getEmail();
+        
     }
 
     String getInfo() {
         // TODO:
-        return null; // xóa dòng này sau khi cài đặt
+        return "Ten: " + name + "\n" + "Ma so SV: " + id + "\n" + "Lop: " + group + "\n" + "Email: " + email;
+      //  return null; // xóa dòng này sau khi cài đặt
     }
 }
