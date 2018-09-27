f4510020393e48c5daa491e5373ef0929f50ffab
ltvinh9899 <37173634+ltvinh9899@users.noreply.github.com>
2018-09-27 10:13:48 +0700

    Update Task1.java

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -6,6 +6,8 @@ public class Task1 {
 
     public static int gcd(int a, int b) {
         // TODO: Tính ước chung lớn nhất của 2 số a, b
+        a = Math.abs(a);
+        b = Math.abs(b);
         if(a == b)
             return a;
         else {
