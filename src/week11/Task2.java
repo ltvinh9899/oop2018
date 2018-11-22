1afed6d9fe4bcb804e97a73365863b193b66515c
ltvinh9899 <37173634+ltvinh9899@users.noreply.github.com>
2018-11-22 09:31:01 +0700

    up

--- src/week11/Task2.java
+++ src/week11/Task2.java
@@ -1,4 +1,22 @@
 package week11;
 
 public class Task2 {
+    public static  <T extends Comparable> T maxArray(T[] a){
+        T max = a[0];
+        for(T s : a){
+            if(s.compareTo(max) > 0){
+                max = s;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Integer [] a = {1, 9, 4, 5};
+        Integer i = maxArray(a);
+        Character [] a_ = {'a', 'd', 'c', 'b'};
+        Character c = maxArray(a_);
+        System.out.println(i + " " + c);
+    }
 }
+
